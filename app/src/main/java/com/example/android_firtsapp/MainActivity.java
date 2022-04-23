eb65483fa65e35d352bca195522d0325b6bbe4a4
Khang Dev <volenhatkhang1808@gmail.com>
2022-04-23 20:26:33 +0700

    Add Hello Chị

--- app/src/main/java/com/example/android_firtsapp/MainActivity.java
+++ app/src/main/java/com/example/android_firtsapp/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
+//Hello Chi
     @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
